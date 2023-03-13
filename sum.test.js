37ffb49ec8f86280b501d016404145d9c50b222a
ulisinaga14 <107898121+ulisinaga14@users.noreply.github.com>
2023-03-13 16:56:51 +0700

    Add files via upload

---
+++ sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+<<<<<<< HEAD
+});
+=======
+});
+>>>>>>> 12f76e2b0f1919e1391cb6fa5bfaab948b764468
